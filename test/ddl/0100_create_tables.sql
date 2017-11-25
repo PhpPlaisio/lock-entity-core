f4098578ffcd9f2c014003196e1cf07d0e5e35e1
Paul Water <p.r.water@setbased.nl>
2017-11-25 05:03:22 +0100

    Aligned with renamed tables.

--- test/ddl/0100_create_tables.sql
+++ test/ddl/0100_create_tables.sql
@@ -1,13 +1,13 @@
-CREATE TABLE `AUT_COMPANY` (
+CREATE TABLE `ABC_AUTH_COMPANY` (
   `cmp_id` SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
   `cmp_abbr` VARCHAR(15) NOT NULL,
   `cmp_label` VARCHAR(20) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
   CONSTRAINT `PRIMARY_KEY` PRIMARY KEY (`cmp_id`)
 );
 
-insert into `AUT_COMPANY`( `cmp_id`
-,                          `cmp_abbr`
-,                          `cmp_label` )
+insert into `ABC_AUTH_COMPANY`( `cmp_id`
+,                               `cmp_abbr`
+,                               `cmp_label` )
 values( 1
 ,       'SYS'
 ,       'CMP_ID_SYS')
